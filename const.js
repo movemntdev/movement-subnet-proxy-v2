c4086de225d54dc15027920380ac1c61a5557569
tony <tony@movemnt.dev>
2023-12-18 10:37:08 +0800

    Refactor: Organize code structure Enhancement: Add support for error handling in subnets

--- const.js
+++
@@ -1,7 +0,0 @@
-const PORT = 3001
-const URL = "https://subnet.devnet.m1.movementlabs.xyz/v1/ext/bc/2vUTKYZBbLtXnfCL2RF5XEChZf1wxVYQqxZQQCShMmseSKSiee/rpc"
-
-module.exports = {
-    URL,
-    PORT
-}
